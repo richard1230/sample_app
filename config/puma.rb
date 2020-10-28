6e54a141aa285e1e03cd481379844a1ddcdc1db8
richard1230 <yongjinhu1230@gmail.com>
2020-10-28 08:37:31 +0000

    Use SSL and the Puma webserver in production-edit config/pima.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-ds_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
     min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
         threads min_threads_count, max_threads_count
 	    port        ENV.fetch("PORT") { 3000 }
